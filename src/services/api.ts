a2715b7e7b049fc62b807fa502405ee26ecc124d
Caio Alvarenga <caioreisalvarenga@gmail.com>
2025-07-21 16:39:59 -0300

    feat: config route api

---
+++ src/services/api.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+interface Viagem {
+  id: number;
+  origem: string;
+  destino: string;
+  dataCriacao: string;
+}
+
+interface ViagemResponse {
+  data: Viagem[];
+  totalPaginas: number;
+}
+
+const apiToken = '2f4c6f15-970d-4a8e-aa99-8daaa6801112'; 
+const API = axios.create({
+  baseURL: 'https://apislhubclient.dev.slhub.com.br/api/viagem',
+  headers: {
+    'api-token': apiToken,
+  },
+});
+
+export const fetchViagens = async (params: any): Promise<ViagemResponse> => {
+  try {
+    const { data } = await API.get('', { params });
+    return {
+      data: data.data || [],
+      totalPaginas: data.totalPaginas || 1,
+    };
+  } catch (err) {
+    console.error('Erro ao buscar viagens:', err);
+    throw err;
+  }
+};
+
+export type { Viagem };
